8a27ffa0256c09f6f6729d59acd91f1b5125fd10
realpj10101 <parsajfr1385oo@gmail.com>
2026-02-01 23:13:56 +0330

    create menu service

---
+++ client/src/app/services/menu.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { MenuRecommedRes, MenuRecommnedReq } from '../models/menu';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  private _http = inject(HttpClient);
+  
+  private readonly _baseApiUrl = environment.apiUrl + 'menu/';
+
+  recommendTalk(req: MenuRecommnedReq): Observable<MenuRecommedRes> {
+    return this._http.post<MenuRecommedRes>(this._baseApiUrl + 'menu-talk', req);
+  }
+}
