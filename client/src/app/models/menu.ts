ca37820f2b11a01141333c644a28db775c3b093a
realpj10101 <parsajfr1385oo@gmail.com>
2026-02-01 23:04:25 +0330

    crearte menu  model

---
+++ client/src/app/models/menu.ts
@@ -0,0 +1,26 @@
+export class MenuRecommnedReq
+{
+    query: string = '';
+    topN: number = 10;
+    topK: number = 3;
+}
+
+export interface MenuRecommedRes
+{
+    query: string;
+    topN: number;
+    messageFa: string;
+    candidates: MenuCandiate[];
+}
+
+export interface MenuCandiate
+{
+    id: string;
+    catgeryNameFa: string;
+    persianName: string;
+    englishName: string;
+    ingredients: string;
+    imageUrl: string;
+    priceValue: number;
+    score: number;
+}
